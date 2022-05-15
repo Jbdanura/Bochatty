7f4209924027e422cad0101dda85053912faa2e0
jotabe <danurabe@gmail.com>
2022-05-15 05:27:03 -0300

    commit

--- public/script.js
+++ public/script.js
@@ -23,7 +23,7 @@ button.addEventListener("click",async function(){
         generateMessage(myMessage,false)
         messages.scrollTop = messages.scrollHeight
         try{
-            const response = await fetch("/message",{
+            const response = await fetch(`${window.location.href}/message`,{
                 method:"POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({
